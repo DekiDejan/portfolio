fa7a8b9da0fb0c59142b95a8ec10ff49e61f272a
Dejan Mironski Panchevski <99611699+DekiDejan@users.noreply.github.com>
2024-10-06 20:24:41 +0200

    add hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import desktopBg from "../assets/desktop bg.png";
+import arrowDown from "../assets/icons/down-arrow 1.svg";
+import githubIcon from "../assets/icons/github.svg";
+import linkedinIcon from "../assets/icons/linkedin.svg";
+import cvIcon from "../assets/icons/cv.svg";
+import cvFile from "../assets/CV - Dejan Mironski Panchevski.pdf";
+
+const Hero: React.FC = () => {
+  return (
+    <div
+      className="h-screen w-full bg-cover bg-center"
+      style={{ backgroundImage: `url(${desktopBg})` }}
+    >
+      <div className="flex h-full mx-36">
+        <div className="basis-1/2 flex flex-col items-center justify-center h-full text-[#3A4231]">
+          <h1 className="text-9xl font-['Caveat'] tracking-[50px] -mr-6">
+            DEJAN
+          </h1>
+          <h2 className="text-7xl font-bold mt-4 mb-12">WEB DEVELOPER</h2>
+          <p className="text-xl text-white font-light opacity-90">
+            With a background in design, I bring creativity and technical
+            expertise to build intuitive, modern web applications
+          </p>
+          <a
+            href="#"
+            className="text-white text-2xl bg-[#3A4231] w-full text-center my-7 py-3"
+          >
+            <span className="opacity-90 mr-4">CONTACT ME</span>
+            <img src={arrowDown} alt="Arrow down icon" className="inline" />
+          </a>
+          <div className="flex gap-8 opacity-90">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/DekiDejan"
+            >
+              <img src={githubIcon} alt="GitHub icon" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/dejan-mironski-panchevski/"
+            >
+              <img src={linkedinIcon} alt="LinkedIn icon" />
+            </a>
+            <a href={cvFile} download="CV - Dejan Mironski Panchevski">
+              <img src={cvIcon} alt="CV icon" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-0 w-full h-[15%] bg-gradient-to-b from-transparent to-[#628340]"></div>
+    </div>
+  );
+};
+
+export default Hero;

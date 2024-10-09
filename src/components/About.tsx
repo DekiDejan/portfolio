69edd58bd7314e9220e990a55f5cbe41ccd52716
Dejan Mironski Panchevski <99611699+DekiDejan@users.noreply.github.com>
2024-10-09 12:17:20 +0200

    add form validation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,32 +1,7 @@
-import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
+import React from "react";
+import Contact from "./Contact";
 
 const About: React.FC = () => {
-  const formElement = useRef<HTMLFormElement>(null);
-
-  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_0stt544",
-        "template_iye4uo8",
-        formElement.current as HTMLFormElement,
-        {
-          publicKey: "Ryh5__SAp4AnHvaq7",
-        }
-      )
-      .then(
-        () => {
-          console.log("SUCCESS!");
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
-    formElement.current?.reset();
-  };
-
   return (
     <div className="pt-12 pb-24 mx-36">
       <h3
@@ -38,9 +13,9 @@ const About: React.FC = () => {
       <div className="flex gap-8 mt-12">
         <div className="basis-1/2">
           <p className="text-white opacity-90 text-xl italic font-light text-justify">
-            &nbsp; I'm Dejan, a front-end web developer who loves creating
+            &nbsp;I'm Dejan, a front-end web developer who loves creating
             websites that are both visually appealing and easy to use. My
-            experience working with React, Next.js, TypeScript, and Tailwind CSS
+            experience working with React, Next.js, TypeScript, and TailwindCSS
             has helped me develop the skills to build modern, user-friendly web
             applications. With a background in design, I pay close attention to
             detail, making sure that my projects are not only functional but
@@ -54,37 +29,11 @@ const About: React.FC = () => {
             On top of that, cycling is another hobby I’m passionate about,
             keeping me active and allowing me to unwind after a day of coding.
             <br />
-            &nbsp;I’m always excited to take on new challenges and connect with
+            &nbsp; I’m always excited to take on new challenges and connect with
             others in the field
           </p>
         </div>
-        <div className="basis-1/2 flex flex-col">
-          <p className="text-white text-xl font-bold uppercase">
-            Feel free to contact me and I will get back to you as soon as I can
-          </p>
-          <form
-            ref={formElement}
-            onSubmit={sendEmail}
-            className="mt-6 flex flex-col gap-6 grow"
-          >
-            <input
-              type="email"
-              name="email"
-              placeholder="Your e-mail"
-              className="p-4 placeholder-black placeholder-opacity-30 text-xl rounded-sm"
-            />
-            <textarea
-              name="message"
-              placeholder="Your message"
-              className="p-4 grow placeholder-black placeholder-opacity-30 text-xl rounded-sm resize-none"
-            />
-            <input
-              type="submit"
-              value="SEND MESSAGE"
-              className="py-4 bg-white text-[#628340] cursor-pointer text-xl rounded-sm"
-            />
-          </form>
-        </div>
+        <Contact />
       </div>
     </div>
   );
---
+++ src/components/Contact.tsx
@@ -0,0 +1,90 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact: React.FC = () => {
+  const [message, setMessage] = useState<JSX.Element>(
+    <p className="text-white text-xl font-bold uppercase">
+      Feel free to contact me and I will get back to you as soon as I can
+    </p>
+  );
+
+  const formElement = useRef<HTMLFormElement>(null);
+  const emailInputElement = useRef<HTMLInputElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+    if (emailInputElement.current?.value.match(emailRegex)) {
+      emailjs
+        .sendForm(
+          "service_0stt544",
+          "template_iye4uo8",
+          formElement.current as HTMLFormElement,
+          {
+            publicKey: "Ryh5__SAp4AnHvaq7",
+          }
+        )
+        .then(
+          () => {
+            console.log("SUCCESS!");
+            setMessage(
+              <p className="text-[#C1FF82] text-xl font-bold uppercase">
+                Your message was successfully sent!
+              </p>
+            );
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+            setMessage(
+              <p className="text-red-700 text-xl font-bold uppercase">
+                There was a problem and your message was not sent!
+              </p>
+            );
+          }
+        );
+
+      formElement.current?.reset();
+    } else {
+      setMessage(
+        <p className="text-red-700 text-xl font-bold uppercase">
+          Please enter a valid e-mail.
+        </p>
+      );
+
+      emailInputElement.current?.focus();
+    }
+  };
+
+  return (
+    <div className="basis-1/2 flex flex-col">
+      {message}
+      <form
+        ref={formElement}
+        onSubmit={sendEmail}
+        className="mt-6 flex flex-col gap-6 grow"
+      >
+        <input
+          ref={emailInputElement}
+          type="email"
+          name="email"
+          placeholder="Your e-mail"
+          className="p-4 placeholder-black placeholder-opacity-30 text-xl rounded-sm"
+        />
+        <textarea
+          name="message"
+          placeholder="Your message"
+          className="p-4 grow placeholder-black placeholder-opacity-30 text-xl rounded-sm resize-none"
+        />
+        <input
+          type="submit"
+          value="SEND MESSAGE"
+          className="py-4 bg-white text-[#628340] cursor-pointer text-xl rounded-sm"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
